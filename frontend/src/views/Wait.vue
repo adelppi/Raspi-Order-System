6956c70166e9dbf59871cdc6828f6e34c3691240
adelppi <adelppi.baig@gmail.com>
2023-09-30 21:59:48 +0900

    fix: show thumbnails added: ad

--- frontend/src/views/Wait.vue
+++ frontend/src/views/Wait.vue
@@ -1,3 +1,18 @@
 <template>
     <h1>届くまで<br>待ってね<br>😋</h1>
+    <div class="ad">
+        <img src="/src/assets/ad.png" >
+    </div>
 </template>
+
+<style scoped>
+.ad {
+    width: 50%;
+    margin-left: auto;
+    margin-right: auto;
+    /* background-color: aquamarine; */
+}
+img {
+    width: 100%;
+}
+</style>
