c9750c4f62b28c2381d778e3f45e57049b3f527e
adelppi <adelppi.baig@gmail.com>
2023-10-28 06:55:46 +0900

    update: MenuSeeder

--- backend/database/seeders/MenuSeeder.php
+++ backend/database/seeders/MenuSeeder.php
@@ -19,35 +19,35 @@ class MenuSeeder extends Seeder
                 'menu_id' => 1,
                 'title' => 'ラズベリーパイ&コーヒー・セット',
                 'description' => 'set',
-                'price' => 500,
+                'price' => 600,
                 'stock' => 50
             ],
             [
                 'menu_id' => 2,
                 'title' => 'ラズベリーパイ&紅茶・セット',
                 'description' => 'set',
-                'price' => 500,
+                'price' => 600,
                 'stock' => 50
             ],
             [
                 'menu_id' => 3,
                 'title' => 'ラズベリーパイ&緑茶・セット',
                 'description' => 'set',
-                'price' => 500,
+                'price' => 600,
                 'stock' => 50
             ],
             [
                 'menu_id' => 4,
                 'title' => 'ラズベリーパイ&三ツ谷サイダー・セット',
                 'description' => 'set',
-                'price' => 500,
+                'price' => 600,
                 'stock' => 50
             ],
             [
                 'menu_id' => 5,
                 'title' => 'ベビーカステラ',
                 'description' => 'food',
-                'price' => 100,
+                'price' => 200,
                 'stock' => 50
             ],
             [
@@ -61,21 +61,21 @@ class MenuSeeder extends Seeder
                 'menu_id' => 7,
                 'title' => '単品・紅茶',
                 'description' => 'drink',
-                'price' => 200,
+                'price' => 150,
                 'stock' => 50
             ],
             [
                 'menu_id' => 8,
                 'title' => '単品・緑茶',
                 'description' => 'drink',
-                'price' => 200,
+                'price' => 100,
                 'stock' => 50
             ],
             [
                 'menu_id' => 9,
                 'title' => '単品・三ツ矢サイダー',
                 'description' => 'drink',
-                'price' => 200,
+                'price' => 100,
                 'stock' => 50
             ],
         ]);
