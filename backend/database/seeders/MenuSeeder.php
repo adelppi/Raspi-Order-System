c3e66f92f605a27c310a280ec2125b43c5298e4a
adelppi <adelppi.baig@gmail.com>
2023-10-28 18:45:17 +0900

    update: MenuSeeder

--- backend/database/seeders/MenuSeeder.php
+++ backend/database/seeders/MenuSeeder.php
@@ -17,28 +17,28 @@ class MenuSeeder extends Seeder
         DB::table('menus')->insert([
             [
                 'menu_id' => 1,
-                'title' => 'ラズベリーパイ&コーヒー・セット',
+                'title' => 'ラズベリーパイ&コーヒー',
                 'description' => 'set',
                 'price' => 600,
                 'stock' => 50
             ],
             [
                 'menu_id' => 2,
-                'title' => 'ラズベリーパイ&紅茶・セット',
+                'title' => 'ラズベリーパイ&紅茶',
                 'description' => 'set',
                 'price' => 600,
                 'stock' => 50
             ],
             [
                 'menu_id' => 3,
-                'title' => 'ラズベリーパイ&緑茶・セット',
+                'title' => 'ラズベリーパイ&緑茶',
                 'description' => 'set',
                 'price' => 600,
                 'stock' => 50
             ],
             [
                 'menu_id' => 4,
-                'title' => 'ラズベリーパイ&三ツ谷サイダー・セット',
+                'title' => 'ラズベリーパイ&三ツ谷サイダー',
                 'description' => 'set',
                 'price' => 600,
                 'stock' => 50
@@ -48,6 +48,27 @@ class MenuSeeder extends Seeder
                 'title' => 'ベビーカステラ',
                 'description' => 'food',
                 'price' => 200,
+                'stock' => 50
+            ],
+            [
+                'menu_id' => 5,
+                'title' => 'ジャム・いちご',
+                'description' => 'jam',
+                'price' => 0,
+                'stock' => 50
+            ],
+            [
+                'menu_id' => 5,
+                'title' => 'ジャム・ブルーベリー',
+                'description' => 'jam',
+                'price' => 0,
+                'stock' => 50
+            ],
+            [
+                'menu_id' => 5,
+                'title' => 'ジャム・マーマレード',
+                'description' => 'jam',
+                'price' => 0,
                 'stock' => 50
             ],
             [
@@ -61,21 +82,28 @@ class MenuSeeder extends Seeder
                 'menu_id' => 7,
                 'title' => '単品・紅茶',
                 'description' => 'drink',
-                'price' => 150,
+                'price' => 200,
                 'stock' => 50
             ],
             [
                 'menu_id' => 8,
                 'title' => '単品・緑茶',
                 'description' => 'drink',
-                'price' => 100,
+                'price' => 200,
                 'stock' => 50
             ],
             [
                 'menu_id' => 9,
+                'title' => 'コーラ',
+                'description' => 'drink',
+                'price' => 200,
+                'stock' => 50
+            ],
+            [
+                'menu_id' => 10,
                 'title' => '単品・三ツ矢サイダー',
                 'description' => 'drink',
-                'price' => 100,
+                'price' => 200,
                 'stock' => 50
             ],
         ]);
